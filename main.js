df90c4ddaaac7d03a94378af91f5f86eed4b8d58
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2022-06-21 21:23:53 -0300

    add-animation

--- main.js
+++ main.js
@@ -30,3 +30,41 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// ========== SCROLLREVEALJS ==========
+
+const scrollRevealTop = {
+  origin: "top",
+  distance: "50px",
+  duration: 1500,
+  reset: true,
+};
+
+const scrollRevealLeft = {
+  origin: "left",
+  distance: "30px",
+  duration: 1500,
+  reset: true,
+};
+
+const scrollRevealRight = {
+  origin: "right",
+  distance: "30px",
+  duration: 1500,
+  reset: true,
+};
+
+ScrollReveal().reveal(
+  `.header-container, .img-meeting-pleople, .introduction, .swiper, .section-three h2, .section-four h2, .section-five h2`,
+  scrollRevealTop
+);
+
+ScrollReveal().reveal(
+  `.introduction, .img-team, .img-history, .section-four .pair, .social-midia`,
+  scrollRevealRight
+);
+
+ScrollReveal().reveal(
+  `.img-meeting-pleople, .message-team, .message-history, .section-four .odd, form`,
+  scrollRevealLeft
+);
